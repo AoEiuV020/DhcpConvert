b61a498094e42e0f215b0f73522e07ecefda82ee
AoEiuV020 <aoeiuv020@gmail.com>
2023-05-11 00:11:34 +0800

    create project

---
+++ bin/dhcp_convert.dart
@@ -0,0 +1,5 @@
+import 'package:dhcp_convert/dhcp_convert.dart' as dhcp_convert;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dhcp_convert.calculate()}!');
+}
---
+++ test/dhcp_convert_test.dart
@@ -0,0 +1,8 @@
+import 'package:dhcp_convert/dhcp_convert.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
