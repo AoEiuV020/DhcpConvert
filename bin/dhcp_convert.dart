5994f4788ca7673cc0ffa10ed0c980ba59d2fabd
AoEiuV020 <aoeiuv020@gmail.com>
2023-08-16 12:26:03 +0800

    使用json5打印结果

--- bin/dhcp_convert.dart
+++ bin/dhcp_convert.dart
@@ -1,16 +1,15 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:dhcp_convert/dhcp.dart';
 import 'package:dhcp_convert/openwrt.dart';
+import 'package:json5/json5.dart';
 
-void main(List<String> arguments) {
+Future<void> main(List<String> arguments) async {
   var file = File("example/dhcp_openwrt.txt");
   print('read: ${file.absolute}!');
   Config dhcp = Openwrt();
   print('type: ${dhcp.name}');
   var result = dhcp.convert(file.openRead());
-  result.forEach((element) {
-    print(json.encode(element.toJson()));
-  });
+  var list = await result.map((event) => event.toJson()).toList();
+  print(JSON5.stringify(list, space: 2));
 }
